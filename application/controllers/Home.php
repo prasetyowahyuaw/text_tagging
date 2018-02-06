8e2fd733d503e2d07e9dc8e80a7376b9452c93e9
adrian <adrianafnandika@gmail.com>
2018-02-06 18:43:15 +0700

    cek

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,7 +12,7 @@ class Home extends CI_Controller
     
     public function index()
     {
-        $this->slice->view('home');
+        $this->slice->view('home2');
     }
 
     public function logout()
